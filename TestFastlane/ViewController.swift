24af30097b17f3cea1d100af7d012928f35ac60a
sujin <sujinchaichanamongkol@gmail.com>
2018-06-07 15:48:33 +0700

    gg

---
+++ TestFastlane/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFastlane
+//
+//  Created by Sujin chaichanamongkol on 7/6/2561 BE.
+//  Copyright © 2561 Sujin chaichanamongkol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
